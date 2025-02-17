7a34e0774c94a1cecc141346abb8d9b037c4ea2d
AlexLoitzl <alexander.loitzl@gmail.com>
2025-02-17 20:20:08 +0100

    Add unfinished looping non-iris proof

--- dimsum_examples/echo_original.v
+++ dimsum_examples/echo_original.v
@@ -54,48 +54,92 @@ Section echo.
     else
       TUb.
 
-  Definition combined_spec : spec rec_event Z void :=
-    '(f, vs, h) ← TReceive (λ '(f, vs, h), (Incoming, ERCall f vs h));
-    TAssume (f = "echo");;
-    TAssume (vs = []);;
+  Definition combined_spec_body : spec rec_event Z unit :=
     v ← TGet;
     TPut (v + 1);;
     h ← TExist _;
     '(_, h') ← TCallRet "putc" [(ValNum v)] h;
-    TAssume (h = h');;
-    TUb.
+    TAssume (h = h').
+
+  Definition combined_spec : spec rec_event Z void :=
+    '(f, vs, h) ← TReceive (λ '(f, vs, h), (Incoming, ERCall f vs h));
+    TAssume (f = "echo");;
+    TAssume (vs = []);;
+    Spec.forever combined_spec_body.
 
 End echo.
 
 Section link.
 
-  Lemma echo_getc_sim :
+  Lemma echo_getc_sim v:
     trefines (rec_link {["echo"]} {["getc"]}
-                (rec_mod echo_prog) (spec_mod getc_spec 0))
-            (spec_mod combined_spec 0).
+                (rec_mod echo_prog) (spec_mod getc_spec v))
+            (spec_mod combined_spec v).
   Proof.
     apply: tsim_implies_trefines => n0 /=.
+    (* apply tsim_remember_all. *)
+    (* intros ?? IH ? => /=. simpl. *)
     tstep_i => *. case_match; destruct!/=.
     go_s. eexists (_, _, _). go.
     go_s. split!. go.
-    go_s => ?. go. go_s => ?. go. simplify_eq. rewrite bool_decide_true; [|done].
+    go_s => ?. go. go_s => ?. intros ?->. simplify_eq. rewrite bool_decide_true; [|done].
     tstep_i. split! => ???? Hf ?. unfold echo_prog in Hf. simplify_map_eq.
-    tstep_i. split! => ? Hf. unfold echo_prog in Hf. simplify_map_eq. split!.
-    tstep_i => *. destruct!/=. split; [by repeat econs|].
+
+    unshelve eapply tsim_remember. { simpl. exact (λ _ '(_, _, (Rec e _ f), (t1, v1)) '(t2, v2),
+      (* TODO: How does Michael think about this, I guess typically I would phrase an induction hypothesis *)
+    (*     using an arbitrary kontext here *)
+      exists K v,
+      t1 ≡ getc_spec ∧
+      e = expr_fill K ((Call (Val (ValFn "echo"))) (Val <$> [])) ∧
+      f = echo_prog ∧
+      t2 ≡ Spec.forever combined_spec_body ∧
+      v1 ≡ v ∧
+      v2 ≡ v). }
+    { eexists [ReturnExtCtx false]. split!. } { done. }
+    move => n _ Hloop a b c.
+    destruct a as [[[aa ab] [ac ac2]] a2].
+    destruct b as [? ?].
+    destruct!/=.
+
+    Compute m_state (rec_link_trans {["echo"]} {["getc"]} rec_trans (spec_trans rec_event Z)).
+    move => n _ Hloop [????]. destruct!/=.
+
+    intros ?? IH ??? => /=.
+    revert v.
+    apply tsim_remember_all.
+    intros ?? IH ? => /=.
+
+    tstep_i. split! => ? Hf. unfold echo_prog in Hf. simplify_map_eq.
+    split!. tstep_i => *. destruct!/=. split!.
     tstep_i. rewrite {1}/echo_prog. split!=> *. case_match; destruct!/=.
     rewrite bool_decide_true; [|done].
     go_i. go_i. intros [[? ?] ?]. go.
     go_i => ?. destruct!/=. go.
     tstep_i. split!. go.
     tstep_i. split!. go.
-    go_i. go_i. go_i => *. go. destruct!/=.
+    go_i. go_i. go_i => *. intros ? Hs.  destruct!/=.
     go_i. split! => *. destruct!/=.
     go_i. go_i. split! => *. destruct!/=. rewrite bool_decide_false; [|done].
-    go_s. go_s. go_s. eexists. go. go_s. split!. go.
+    go_s. go_s. go_s. go_s. eexists. go. go_s. split!. go.
     go_i => e *. destruct!/=. destruct e; destruct!/=.
     { go_s. split!. go. go_s. split!. go. go_s. exact I. }
     go_s. split!. go. go_s. split!. go. go_s => ?. go.
-    by go_s.
-Qed.
+    tstep_s. intros ?->  => /=.
+    tstep_i. split!. intros. tstep_i. simplify_eq.
+    rewrite -unfold_forever => /=. rewrite -/getc_spec in Hs.
+
+    eapply IH.
+    set HIDE := (X in FreeA _ X).
+    eapply tstep_i_generic.
+    apply IH.
+
+
+    go_s. simplify_eq.
+    go_i. split!. intros. go_i. simplify_eq.
+
+    apply IH.
+    done.
+    tstep
+  Qed.
 
 End link.
