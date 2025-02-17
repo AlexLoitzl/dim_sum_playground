f354ab8e48107c095b0f7be5d5ecc17342e04c50
AlexLoitzl <alexander.loitzl@gmail.com>
2025-02-17 15:42:18 +0100

    Do final proof in og dim sum

---
+++ dimsum_examples/echo_original.v
@@ -0,0 +1,209 @@
+From dimsum.core Require Export proof_techniques.
+From dimsum.core Require Import spec_mod.
+From dimsum.examples Require Import rec asm rec_to_asm print.
+From dimsum.examples.compiler Require Import compiler.
+
+Local Open Scope Z_scope.
+
+Local Ltac go :=
+  clear_spec.
+Local Ltac go_s :=
+  tstep_s; go.
+Local Ltac go_i :=
+  tstep_i; go.
+
+Section getc.
+
+  Definition getc_spec : spec rec_event Z void :=
+    Spec.forever(
+    (* Incoming call of getc *)
+    '(f, vs, h) ← TReceive (λ '(f, vs, h), (Incoming, ERCall f vs h));
+    TAssume (f = "getc");;
+    TAssume (vs = []);;
+    (* There is a current position of the buffer *)
+    v ← TGet;
+    TPut (v + 1);;
+    (* Return old position *)
+    TVis (Outgoing, ERReturn (ValNum v) h)).
+
+End getc.
+
+Section echo.
+
+  Definition echo_rec : fndef := {|
+    fd_args := [];
+    fd_static_vars := [];
+    fd_vars := [];
+    fd_body := LetE "c" (rec.Call (Val (ValFn "getc")) []) $
+              LetE "_" (rec.Call (Val (ValFn "putc")) [Var "c"]) $
+              rec.Call (Val (ValFn "echo")) [];
+    fd_static := I
+  |}.
+
+  Definition echo_prog : gmap string fndef :=
+    <["echo" := echo_rec]> $ ∅.
+
+  (* Call with direct return *)
+  Definition TCallRet {S} (f : string) (vs : list val) (h : heap_state) :
+    spec rec_event S (val * heap_state) :=
+    TVis (Outgoing, ERCall f vs h);;
+    e ← TExist _;
+    TVis (Incoming, e);;
+    if e is ERReturn v h' then
+      TRet (v, h')
+    else
+      TUb.
+
+  Definition combined_spec : spec rec_event Z void :=
+    '(f, vs, h) ← TReceive (λ '(f, vs, h), (Incoming, ERCall f vs h));
+    TAssume (f = "echo");;
+    TAssume (vs = []);;
+    v ← TGet;
+    TPut (v + 1);;
+    h ← TExist _;
+    '(_, h') ← TCallRet "putc" [(ValNum v)] h;
+    TAssume (h = h');;
+    TUb.
+
+End echo.
+
+Section link.
+
+  Lemma echo_getc_sim :
+    trefines (rec_link {["echo"]} {["getc"]}
+                (rec_mod echo_prog) (spec_mod getc_spec 0))
+            (spec_mod combined_spec 0).
+  Proof.
+    apply: tsim_implies_trefines => n0 /=.
+    tstep_i => *. case_match; destruct!/=.
+    go_s. eexists (_, _, _). go.
+    go_s. split!. go.
+    go_s => ?. go. go_s => ?. go. simplify_eq. rewrite bool_decide_true; [|done].
+    tstep_i. split! => ???? Hf ?. unfold echo_prog in Hf. simplify_map_eq.
+    tstep_i. split! => ? Hf. unfold echo_prog in Hf. simplify_map_eq. split!.
+    tstep_i => *. destruct!/=. split; [by repeat econs|].
+    tstep_i. rewrite {1}/echo_prog. split!=> *. case_match; destruct!/=.
+    rewrite bool_decide_true; [|done].
+    go_i. go_i. intros [[? ?] ?]. go.
+    go_i => ?. destruct!/=. go.
+    tstep_i. split!. go.
+    tstep_i. split!. go.
+    go_i. go_i. go_i => *. go. destruct!/=.
+    go_i. split! => *. destruct!/=.
+    go_i. go_i. split! => *. destruct!/=. rewrite bool_decide_false; [|done].
+    go_s. go_s. go_s. eexists. go. go_s. split!. go.
+    go_i => e *. destruct!/=. destruct e; destruct!/=.
+    { go_s. split!. go. go_s. split!. go. go_s. exact I. }
+    go_s. split!. go. go_s. split!. go. go_s => ?. go.
+    by go_s.
+Qed.
+    tstep_i. go.
+    go.
+
+    rewrite offset_loc_0.
+    have ? : l.2 = 0 by destruct select (heap_is_fresh _ _) as [?[??]].
+    tstep_i.
+    split.
+    { eapply heap_alive_alloc; [done|lia]. }
+    tstep_i.
+    tstep_i.
+    tstep_i.
+    split.
+    { eapply heap_alive_update. eapply heap_alive_alloc; simpl; [done|lia]. }
+    tstep_i.
+    tstep_i.
+    tstep_i.
+    tstep_i.
+    split! => *. destruct!/=.
+    rewrite bool_decide_false; [|compute_done].
+    rewrite bool_decide_true; [|compute_done].
+    rewrite offset_loc_0 /=.
+
+    tstep_i. rewrite -/memmove_spec. go.
+    go_i => -[[??]?]. go.
+    go_i => ?. go. simplify_eq/=.
+    go_i. split!. go.
+    go_i. eexists (_, _, _). go.
+    go_i. split!. go.
+    go_i. eexists ([ValNum 1; ValNum 2]). go.
+    go_i. split!. go.
+    go_i. split!.
+    { move => -[|[|//]] /= ??; simplify_eq; rewrite ?offset_loc_0.
+      - rewrite lookup_alter_ne.
+        2: { destruct l. rewrite /offset_loc /=. naive_solver lia. }
+        by rewrite lookup_alter heap_alloc_h_lookup/=; [..|lia].
+      - rewrite lookup_alter lookup_alter_ne.
+        2: { destruct l. rewrite /offset_loc /=. naive_solver lia. }
+        by rewrite heap_alloc_h_lookup/=; [..|lia].
+    }
+    go.
+
+    go_i. split!. {
+      move => i ??. do 2 apply heap_alive_update.
+      apply heap_alive_alloc => //=. destruct i as [|[|]] => //; lia.
+    }
+    go.
+
+    go_i => ??????. destruct!. go.
+    go_i. split! => *. destruct!.
+    rewrite !kmap_insert kmap_empty.
+    tstep_i.
+    tstep_i.
+    tstep_i.
+    exists (ValNum 1). split. {
+      rewrite !map_union_weak_insert map_union_weak_empty.
+      rewrite offset_loc_0 lookup_alter lookup_alter_ne.
+      2: { destruct l. rewrite /offset_loc /=. naive_solver lia. }
+      rewrite lookup_alter lookup_alter_ne.
+      2: { destruct l. rewrite /offset_loc /=. naive_solver lia. }
+      by rewrite heap_alloc_h_lookup/=; [..|lia].
+    }
+    tstep_i. split! => *. destruct!.
+    rewrite bool_decide_false; [|compute_done].
+    go_s. split!. go.
+    go_s. split!. go.
+    tstep_i => e *. destruct!.
+
+    destruct e; destruct!.
+    { go_s. split!. go.
+      go_s. split!. go.
+      go_s. split!.
+    }
+    go_s. split!. go.
+    go_s. split!. go.
+    tstep_i.
+    split! => *. simplify_eq.
+    tstep_i.
+    tstep_i.
+    go_s => *. subst. go.
+    tstep_i. split!.  {
+      rewrite !map_union_weak_insert map_union_weak_empty.
+      rewrite offset_loc_0 lookup_alter_ne.
+      2: { destruct l. rewrite /offset_loc /=. naive_solver lia. }
+      rewrite offset_loc_assoc lookup_alter lookup_alter_ne.
+      2: { destruct l. rewrite /offset_loc /=. naive_solver lia. }
+      rewrite lookup_alter_ne.
+      2: { destruct l. rewrite /offset_loc /=. naive_solver lia. }
+      by rewrite heap_alloc_h_lookup/=; [..|lia].
+    }
+    tstep_i. split!. intros. destruct!.
+    rewrite bool_decide_false; [|compute_done].
+    go_s. split!. go.
+    go_s. split!. go.
+    tstep_i => e *. destruct!.
+    destruct e; destruct!.
+    { go_s. split!. go.
+      go_s. split!. go.
+      go_s. split!.
+    }
+    go_s. split!. go.
+    go_s. split!. go.
+    tstep_i.
+    split!. intros. simplify_eq.
+    tstep_i.
+    go_s. intros. subst. go.
+    go_s. done.
+  Qed.
+
+
+End link.
