a2ee1ae29dbeafa0ec4f358424f6f6b11e7c7aa3
AlexLoitzl <alexander.loitzl@gmail.com>
2025-03-10 09:13:30 +0100

    Fix assumption

--- dimsum_examples/iris/final_echo.v
+++ dimsum_examples/iris/final_echo.v
@@ -98,15 +98,15 @@ Section sim_getc.
   Qed.
 
   Lemma sim_getc_spec `{!specGS} Π Φ :
-    □ switch Π ({{ κ σ1 POST,
-        ∃ f es h, ⌜κ = Some (Incoming, ERCall f es h)⌝ ∗
-      POST Tgt _ _ ({{ σ' Π',
-        ∃ v, ⌜f = "getc"⌝ ∗ ⌜es = []⌝ ∗ spec_state v ∗ ⌜σ' = σ1⌝ ∗
-      switch Π' (λ κ (σ : m_state (spec_trans rec_event Z)) POST,
-        ⌜κ = Some (Outgoing, ERReturn (ValNum v) h)⌝ ∗ spec_state (v + 1) ∗
-      POST Tgt _ _ ({{σ' Π'',
-        ⌜σ' = σ⌝ ∗ ⌜Π'' = Π⌝ ∗ TGT getc_spec @ Π {{ Φ }}
-      }}))}})}}) -∗
+    switch Π ({{ κ σ1 POST,
+      ∃ f es h, ⌜κ = Some (Incoming, ERCall f es h)⌝ ∗
+    POST Tgt _ _ ({{ σ' Π',
+      ∃ v, ⌜f = "getc"⌝ ∗ ⌜es = []⌝ ∗ spec_state v ∗ ⌜σ' = σ1⌝ ∗
+    switch Π' (λ κ (σ : m_state (spec_trans rec_event Z)) POST,
+      ⌜κ = Some (Outgoing, ERReturn (ValNum v) h)⌝ ∗ spec_state (v + 1) ∗
+    POST Tgt _ _ ({{σ' Π'',
+      ⌜σ' = σ⌝ ∗ ⌜Π'' = Π⌝ ∗ TGT getc_spec @ Π {{ Φ }}
+    }}))}})}}) -∗
     TGT getc_spec @ Π {{ Φ }}.
   Proof.
     iIntros "#Hs".
